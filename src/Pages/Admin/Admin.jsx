47ea809617b0d03e941eed8433e61b0db1e07127
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-06-24 22:06:27 +0300

    mobx

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,225 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import { getDatabase, ref, push, set, child, get } from 'firebase/database';
+import { observer } from 'mobx-react';
+import { membersStore } from '../../servises/membersStore.jsx';
 import useFirebaseData from '../../firebase/useFirebaseData.js';
 
 import "./Admin.scss";
 
-const Admin = () => {
+const Admin = observer(() => {
+    useEffect(() => {
+        membersStore.fetchLastMemberId();
+    }, []); 
+    
     const data = useFirebaseData();
-    const [user, setUser] = useState({
-        companies: [],
-        directions: [],
-        firstName: '',
-        hobbies: [],
-        id: '',
-        lastName: '',
-        photo: '',
-        quote: '',
-        skills: [],
-        social: [],
-    });
 
-    const [company, setCompany] = useState({
-        id: '',
-        name: '',
-        status: ''
-    });
-    const [direction, setDirection] = useState({
-        id: '',
-        name: '',
-        status: ''
-    });
-    const [skill, setSkill] = useState({
-        name: '',
-        status: ''
-    });
-
-    const [social, setSocial] = useState({
-        link: '',
-        type: ''
-    });
-
+    // Обработчики изменений для полей ввода
     const handleInputChange = (event) => {
         const { name, value } = event.target;
-        setUser({ ...user, [name]: value });
+        membersStore.setMember(name, value);
     };
 
+    // Обработчики изменений для статуса компании
     const handleCompanyStatusChange = (event) => {
         const { value } = event.target;
-        setCompany(prevState => ({
-            ...prevState,
-            status: value
-        }));
+        membersStore.setCompany('status', value);
     };
 
-    const [addedCompanies, setAddedCompanies] = useState([]);
     // Функция для добавления компании
     const handleAddCompany = () => {
-        if (company.id && company.status) {
-            setAddedCompanies(prev => [...prev, { id: company.id, name: company.name, status: company.status }]);
-            // Сбросить текущее состояние компании
-            setCompany({ id: '', status: '' });
-        }
+        membersStore.addCompany();
     };
+
+    // Обработчики изменений для статуса направления
     const handleDirectionStatusChange = (event) => {
         const { value } = event.target;
-        setDirection(prevState => ({
-            ...prevState,
-            status: value
-        }));
+        membersStore.setDirection('status', value);
     };
 
-    const [addedDirections, setAddedDirections] = useState([]);
     // Функция для добавления направления
     const handleAddDirections = () => {
-        if (direction.id && direction.status) {
-            setAddedDirections(prev => [...prev, { id: direction.id, name: direction.name, status: direction.status }]);
-            // Сбросить текущее состояние направления
-            setDirection({ id: '', name: '', status: '' });
-        }
+        membersStore.addDirection();
     };
 
+    // Обработчики изменений для навыков
     const handleSkillChange = (event) => {
         const { name, value } = event.target;
-        setSkill({ ...skill, [name]: value });
+        membersStore.setSkill(name, value);
     };
+
     const handleSkillStatusChange = (event) => {
         const { value } = event.target;
-        setSkill(prevState => ({
-            ...prevState,
-            status: value
-        }));
+        membersStore.setSkill('status', value);
     };
 
-    const [addedSkills, setAddedSkills] = useState([]);
     // Функция для добавления навыка
     const handleAddSkill = () => {
-        if (skill.name && skill.status) {
-            setAddedSkills(prevSkills => [...prevSkills, skill]);
-            // Сбросить текущее состояние навыка
-            setSkill({ name: '', status: '' });
-        }
+        membersStore.addSkill();
     };
 
-    // Обработчик изменения для чекбоксов хобби
-const handleHobbyCheckboxChange = (hobbyId) => {
-    setUser(prevUser => {
-        // Проверяем, содержит ли массив hobbies уже этот hobbyId
-        const isAlreadySelected = prevUser.hobbies.includes(hobbyId);
-        let newHobbies;
-        if (isAlreadySelected) {
-            // Если да, удаляем его из массива
-            newHobbies = prevUser.hobbies.filter(id => id !== hobbyId);
-        } else {
-            // Если нет, добавляем его в массив
-            newHobbies = [...prevUser.hobbies, hobbyId];
-        }
-        return {
-            ...prevUser,
-            hobbies: newHobbies
-        };
-    });
-};
-
-const [addedSocials, setAddedSocials] = useState([]);
-
-const handleSocialChange = (event) => {
-    const { name, value } = event.target;
-    setSocial({ ...social, [name]: value });
-};
-
-const handleAddSocial = () => {
-    if (social.link && social.type) {
-        setAddedSocials(prevSocials => [...prevSocials, { ...social }]);
-        // Сбросить текущее состояние социальной сети
-        setSocial({ link: '', type: '' });
-    }
-};
-
-    // Функция для получения последнего id в списке пользователей
-    const getLastMemberId = async () => {
-        const database = getDatabase();
-        const databaseRef = ref(database, 'members');
-        const snapshot = await get(databaseRef);
-        const membersArray = snapshot.val() ? Object.values(snapshot.val()) : [];
-        const lastId = membersArray.length > 0 ? Math.max(...membersArray.map(member => Number(member.id))) : 0;
-        return lastId;
+    // Функция для добавления социальной сети
+    const handleAddSocial = () => {
+        membersStore.addSocial();
     };
 
-    useEffect(() => {
-        // Запрос к базе данных для получения последних ID при монтировании компонента
-        const fetchLastIds = async () => {
-            const lastMemberId = await getLastMemberId();
-            setLastMemberId(lastMemberId);
-        };
-
-        fetchLastIds();
-    }, []);
-
-    const [lastMemberId, setLastMemberId] = useState(0);
-    // Функция для добавления данных пользователя с уникальным id
-    const addMember = async (memberData) => {
-        const newId = lastMemberId + 1;
-        setLastMemberId(newId);
-        const database = getDatabase();
-        const databaseRef = ref(database, `members/${newId}`);
-        try {
-            await set(databaseRef, {
-                ...memberData,
-                id: newId
-            });
-            console.log(`Пользователь добавлен с ID: ${newId}`);
-        } catch (error) {
-            console.error("Ошибка при добавлении пользователя: ", error);
-        }
+    // Обработчик отправки формы
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        membersStore.submitMember();
     };
 
-
-const handleSubmit = (event) => {
-    event.preventDefault();
-    // Создаем новый массив, содержащий только id и status каждой компании
-    const companiesWithOnlyIdAndStatus = addedCompanies.map(company => ({
-        id: company.id,
-        status: company.status
-    }));
-        // Создаем новый массив, содержащий только id и status каждого направления
-    const directionsWithOnlyIdAndStatus = addedDirections.map(direction => ({
-        id: direction.id,
-        status: direction.status
-    }));
-    // Обновляем объект user перед выводом в консоль
-    setUser(prevUser => ({
-        ...prevUser,
-        companies: companiesWithOnlyIdAndStatus,
-        directions: directionsWithOnlyIdAndStatus,
-        skills: addedSkills,
-        social: addedSocials,
-    }));
-};
-
-useEffect(() => {
-    console.log(user);
-    if (user.companies.length > 0 && user.directions.length > 0 && user.skills.length > 0 && user.social.length > 0) {
-        addMember(user);
-                // Сброс состояний всех полей
-                setUser({
-                    firstName: '',
-                    lastName: '',
-                    id: '',
-                    photo: '',
-                    quote: '',
-                    companies: [],
-                    hobbies: [],
-                    directions: [],
-                    social: [],
-                    skills: [],
-                });
-                setAddedCompanies([]);
-                setAddedDirections([]);
-                setAddedSkills([]);
-                setAddedSocials([]);
-    }
-  }, [user]); // Зависимость от user гарантирует, что код выполнится при его изменении
+    const handlePhotoChange = (event) => {
+        const { value } = event.target;
+        membersStore.setPhoto(value);
+    };
 
     return (
         <main className="admin__container">
@@ -234,7 +85,7 @@ useEffect(() => {
                 <input
                 type="text"
                 name="firstName"
-                value={user.firstName}
+                value={membersStore.member.firstName}
                 onChange={handleInputChange}
                 />
                 <label>
@@ -243,13 +94,13 @@ useEffect(() => {
                 <input
                     type="text"
                     name="lastName"
-                    value={user.lastName}
+                    value={membersStore.member.lastName}
                     onChange={handleInputChange}
                 />
                 <label>
                     Компания:
                 </label>
-                {addedCompanies.map((comp, index) => (
+                {membersStore.addedCompanies.map((comp, index) => (
                     <div key={index}>
                         <span>{comp.name}</span> : <span>{comp.status}</span>
                     </div>
@@ -259,7 +110,8 @@ useEffect(() => {
                     onChange={(event) => {
                         const selectedCompany = data.companies.find(company => company.id === +event.target.value);
                         if (selectedCompany) {
-                            setCompany(prev => ({ ...prev, id: selectedCompany.id, name: selectedCompany.name }));
+                            membersStore.setCompany('id', selectedCompany.id);
+                            membersStore.setCompany('name', selectedCompany.name);
                         } else {
                             // Обработка случая, когда компания не найдена
                             console.error('Выбранная компания не найдена');
@@ -277,7 +129,7 @@ useEffect(() => {
                         type="radio"
                         name="companyStatus"
                         value="Работает"
-                        checked={company.status === 'Работает'}
+                        checked={membersStore.company.status === 'Работает'}
                         onChange={handleCompanyStatusChange}
                     />
                     Работает
@@ -287,7 +139,7 @@ useEffect(() => {
                         type="radio"
                         name="companyStatus"
                         value="Работала"
-                        checked={company.status === 'Работала'}
+                        checked={membersStore.company.status === 'Работала'}
                         onChange={handleCompanyStatusChange}
                     />
                     Работала
@@ -296,7 +148,7 @@ useEffect(() => {
                 <label>
                     Направление:
                 </label>
-                {addedDirections.map((direction, index) => (
+                {membersStore.addedDirections.map((direction, index) => (
                     <div key={index}>
                         <span>{direction.name}</span> : <span>{direction.status}</span>
                     </div>
@@ -306,25 +158,25 @@ useEffect(() => {
                     onChange={(event) => {
                         const selectedDirection = data.directions.find(direction => direction.id === +event.target.value);
                         if (selectedDirection) {
-                            setDirection(prev => ({ ...prev, id: selectedDirection.id, name: selectedDirection.name }));
+                            membersStore.setDirection('id', selectedDirection.id);
+                            membersStore.setDirection('name', selectedDirection.name);
                         } else {
-                            // Обработка случая, когда направление не найдено
                             console.error('Выбранное направление не найдено');
                         }
                     }}
                 >
-                {data.directions.map((direction) => (
-                    <option key={direction.id} value={direction.id}>
-                    {direction.name}
-                    </option>
-                ))}
+                    {data.directions.map((direction) => (
+                        <option key={direction.id} value={direction.id}>
+                            {direction.name}
+                        </option>
+                    ))}
                 </select>
                 <label>
                     <input
                         type="radio"
                         name="directionStatus"
                         value="Уже знает"
-                        checked={direction.status === 'Уже знает'}
+                        checked={membersStore.direction.status === 'Уже знает'}
                         onChange={handleDirectionStatusChange}
                     />
                     Уже знает
@@ -334,7 +186,7 @@ useEffect(() => {
                         type="radio"
                         name="directionStatus"
                         value="Хочет изучить"
-                        checked={direction.status === 'Хочет изучить'}
+                        checked={membersStore.direction.status === 'Хочет изучить'}
                         onChange={handleDirectionStatusChange}
                     />
                     Хочет изучить
@@ -343,7 +195,7 @@ useEffect(() => {
                 <label>
                     Навыки:
                 </label>
-                {addedSkills.map((skill, index) => (
+                {membersStore.addedSkills.map((skill, index) => (
                     <div key={index}>
                         <span>{skill.name}</span> : <span>{skill.status}</span>
                     </div>
@@ -352,7 +204,7 @@ useEffect(() => {
                     <input
                         type="text"
                         name="name"
-                        value={skill.name}
+                        value={membersStore.skill.name}
                         onChange={handleSkillChange}
                     />
                 </label>
@@ -361,7 +213,7 @@ useEffect(() => {
                         type="radio"
                         name="skillStatus"
                         value="Уже знает"
-                        checked={skill.status === 'Уже знает'}
+                        checked={membersStore.skill.status === 'Уже знает'}
                         onChange={handleSkillStatusChange}
                     />
                     Уже знает
@@ -371,7 +223,7 @@ useEffect(() => {
                         type="radio"
                         name="skillStatus"
                         value="Хочет изучить"
-                        checked={skill.status === 'Хочет изучить'}
+                        checked={membersStore.skill.status === 'Хочет изучить'}
                         onChange={handleSkillStatusChange}
                     />
                     Хочет изучить
@@ -384,8 +236,8 @@ useEffect(() => {
                     <label key={hobby.id}>
                         <input
                             type="checkbox"
-                            checked={user.hobbies.includes(hobby.id)}
-                            onChange={() => handleHobbyCheckboxChange(hobby.id)}
+                            checked={membersStore.memberHobbies.includes(hobby.id)}
+                            onChange={() => membersStore.toggleHobby(hobby.id)}
                         />
                         {hobby.name}
                     </label>
@@ -396,21 +248,23 @@ useEffect(() => {
                 <input
                     type="text"
                     name="quote"
-                    value={user.quote}
+                    value={membersStore.member.quote}
                     onChange={handleInputChange}
                 />
                 <label>
                     Социальная сеть:
                 </label>
-                {addedSocials.map((social, index) => (
+                {membersStore.addedSocials.map((social, index) => (
                     <div key={index}>
                         <span>{social.type}</span> : <span>{social.link}</span>
                     </div>
                 ))}
                 <select
                     name="type"
-                    value={social.type}
-                    onChange={handleSocialChange}
+                    value={membersStore.social.type}
+                    onChange={(event) => {
+                        membersStore.setSocial('type', event.target.value);
+                    }}
                 >
                     <option value="telegram">telegram</option>
                     <option value="instagram">instagram</option>
@@ -425,8 +279,10 @@ useEffect(() => {
                 <input
                     type="text"
                     name="link"
-                    value={social.link}
-                    onChange={handleSocialChange}
+                    value={membersStore.social.link}
+                    onChange={(event) => {
+                        membersStore.setSocial('link', event.target.value);
+                    }}
                 />
                 </label>
                 <button type="button" onClick={handleAddSocial}>Добавить соц.сеть</button>
@@ -436,23 +292,24 @@ useEffect(() => {
                         type="text"
                         placeholder="Введите URL фотографии"
                         name="photo"
-                        onChange={handleInputChange}
+                        value={membersStore.member.photo}
+                        onChange={handlePhotoChange}
                     />
                 </label>
                 <button
                     type="submit"
                     disabled=
-                        {!user.firstName 
-                        || !user.lastName 
-                        || addedCompanies.length === 0 
-                        || addedDirections.length === 0 
-                        || addedSkills.length === 0 
-                        || addedSocials.length === 0}
+                        {!membersStore.member.firstName 
+                        || !membersStore.member.lastName 
+                        || membersStore.addedCompanies.length === 0 
+                        || membersStore.addedDirections.length === 0 
+                        || membersStore.addedSkills.length === 0 
+                        || membersStore.addedSocials.length === 0}
                 >
                     Сохранить
                 </button>
             </form>
         </main>
     );
-};
+});
 export default Admin;
---
+++ src/servises/membersStore.jsx
@@ -0,0 +1,163 @@
+import { makeAutoObservable, toJS } from 'mobx';
+import { getDatabase, ref, set, get } from 'firebase/database';
+
+class MembersStore {
+    lastMemberId = 0;
+    member = {
+        companies: [],
+        directions: [],
+        firstName: '',
+        hobbies: [],
+        id: '',
+        lastName: '',
+        photo: '',
+        quote: '',
+        skills: [],
+        social: [],
+    };
+    
+    company = {
+        id: '',
+        name: '',
+        status: ''
+    };
+    
+    direction = {
+        id: '',
+        name: '',
+        status: ''
+    };
+    
+    skill = {
+        name: '',
+        status: ''
+    };
+    
+    social = {
+        link: '',
+        type: ''
+    };
+    
+    addedCompanies = [];
+    addedDirections = [];
+    addedSkills = [];
+    addedSocials = [];
+    memberHobbies = [];
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    setMember = (name, value) => {
+        this.member = { ...this.member, [name]: value };
+    };
+    
+    setCompany = (name, value) => {
+        this.company = { ...this.company, [name]: value };
+    };
+
+    addCompany = () => {
+        if (this.company.id && this.company.status) {
+        this.addedCompanies.push({ ...this.company });
+        this.company = { id: '', name: '', status: '' };
+        }
+    };
+
+    setDirection = (name, value) => {
+        this.direction = { ...this.direction, [name]: value };
+    };
+
+    addDirection = () => {
+        if (this.direction.id && this.direction.status) {
+            this.addedDirections.push({ ...this.direction });
+            this.direction = { id: '', name: '', status: '' };
+        }
+    };
+
+    setSkill = (name, value) => {
+        this.skill = { ...this.skill, [name]: value };
+    };
+
+    addSkill = () => {
+        if (this.skill.name && this.skill.status) {
+            this.addedSkills.push({ ...this.skill });
+            this.skill = { name: '', status: '' };
+        }
+    };
+
+    setSocial = (name, value) => {
+        this.social = { ...this.social, [name]: value };
+    };
+
+    addSocial = () => {
+        if (this.social.link && this.social.type) {
+            this.addedSocials.push({ ...this.social });
+            this.social = { link: '', type: '' };
+        }
+    };
+
+    setPhoto = (photoUrl) => {
+        this.member = { ...this.member, photo: photoUrl };
+    };
+
+    toggleHobby(hobbyId) {
+        const isAlreadySelected = this.memberHobbies.includes(hobbyId);
+        if (isAlreadySelected) {
+            // Если хобби уже выбрано, удаляем его из массива
+            this.memberHobbies = this.memberHobbies.filter(id => id !== hobbyId);
+        } else {
+            // Если хобби не выбрано, добавляем его в массив
+            this.memberHobbies.push(hobbyId);
+        }
+    }
+    
+    submitMember = () => {
+        // Логика для обработки и отправки данных пользователя
+        const member = {
+            firstName: this.member.firstName,
+            lastName: this.member.lastName,
+            photo: this.member.photo,
+            quote: this.member.quote,
+            companies: toJS(this.addedCompanies),
+            hobbies: toJS(this.memberHobbies),
+            directions: toJS(this.addedDirections),
+            social: toJS(this.addedSocials),
+            skills: toJS(this.addedSkills),
+        }
+        console.log(member);
+        this.addMember(member);
+    };
+
+    // Метод для получения последнего ID пользователя из базы данных
+    fetchLastMemberId = async () => {
+        const database = getDatabase();
+        const databaseRef = ref(database, 'members');
+        const snapshot = await get(databaseRef);
+        const membersArray = snapshot.val() ? Object.values(snapshot.val()) : [];
+        const lastId = membersArray.length > 0 ? Math.max(...membersArray.map(member => Number(member.id))) : 0;
+        this.setLastMemberId(lastId);
+    };
+
+    // Действие для установки последнего ID пользователя
+    setLastMemberId = (newId) => {
+        this.lastMemberId = newId;
+    };
+
+    addMember = async (memberData) => {
+        const newId = this.lastMemberId + 1;
+        this.setLastMemberId(newId);
+        const database = getDatabase();
+        const databaseRef = ref(database, `members/${newId}`);
+        try {
+            await set(databaseRef, {
+                ...memberData,
+                id: newId
+            });
+            console.log(`Пользователь добавлен с ID: ${newId}`);
+        } catch (error) {
+            console.error("Ошибка при добавлении пользователя: ", error);
+        }
+    };
+}
+
+export const membersStore = new MembersStore();
